8c08a9bc3801a062f5d66ce1297c8e05eecf88f6
Adam Maracz <adam.maracz@me.com>
2022-05-09 20:53:48 +0200

    Added Pixi, Pixi Plus and OS version fixes

--- enyo-app/source/data/data.js
+++ enyo-app/source/data/data.js
@@ -13,17 +13,43 @@ this.data = {
 				{"name":"Max OS","value":"webOS 1.4.5"},
 			]
 		},
+		{
+			name: "Palm Pixi",
+			icon: "assets/palmpixi-icon.png",
+			photo: "assets/palmpixi.jpg",
+			specs: [
+				{"name":"Screen Size","value":"2.63 inches"},
+				{"name":"Screen Resolution","value":"320x400"},
+				{"name":"RAM","value":"256MB"},
+				{"name":"Storage","value":"8GB"},
+				{"name":"Default OS","value":"webOS 1.2.9.1"},
+				{"name":"Max OS","value":"webOS 1.4.5.1"},
+			]
+		},
+		{
+			name: "Palm Pixi Plus",
+			icon: "assets/palmpixi-icon.png",
+			photo: "assets/palmpixi.jpg",
+			specs: [
+				{"name":"Screen Size","value":"2.63 inches"},
+				{"name":"Screen Resolution","value":"320x400"},
+				{"name":"RAM","value":"256MB"},
+				{"name":"Storage","value":"8GB"},
+				{"name":"Default OS","value":"webOS 1.4.1.2"},
+				{"name":"Max OS","value":"webOS 1.4.5.1"},
+			]
+		},
 		{ 
 			name: "Palm Pre Plus",
-			icon: "assets/palmpre-icon.png",
-			photo: "assets/palmpre.jpg",
+			icon: "assets/palmpreplus-icon.png",
+			photo: "assets/palmpreplus.jpg",
 			specs: [
 				{"name":"Screen Size","value":"3.1 inches"},
 				{"name":"Screen Resolution","value":"320x480"},
 				{"name":"RAM","value":"512MB"},
 				{"name":"Storage","value":"16GB"},
-				{"name":"Default OS","value":"webOS 1.0.1"},
-				{"name":"Max OS","value":"webOS 1.4.5"},
+				{"name":"Default OS","value":"webOS 1.4.2"},
+				{"name":"Max OS","value":"webOS 2.1.0"},
 			]
 		},
 		{ 
